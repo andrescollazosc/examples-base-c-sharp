db344ecc07d417d532eb51b2043be27bc99b2843
Alejandro Martinez <Alejandro.Martinez.-ND@disney.com>
2022-09-27 18:07:02 -0500

    Adding directory TiendaPagoCodilityTest

---
+++ TiendaPagoCodilityTest/Program.cs
@@ -0,0 +1,30 @@
+﻿public class Program
+{
+    public static void Main(string[] args) {
+        Console.WriteLine(Solution("4 5 6 - 7 +"));
+    }
+
+    private static int Solution(string S)
+    {
+        var a = S.Split(' ');
+
+        foreach (var item in a)
+        {
+            switch (item)
+            {
+                case "POP":
+                    
+                    break;
+                case "DUP":// duplica el elemento anterior de la pila
+                    //a.GetValue(a.)
+                    break;
+                case "+":
+                    break;
+                case "-":
+                    break;
+            }
+        }
+
+        return a.Length;
+    }
+}
