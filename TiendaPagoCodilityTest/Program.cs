0c0f18581c1f81c58cc68ddaad01a9a6cfea8da6
bamartinezd <alejandromardelg@gmail.com>
2022-09-27 20:32:45 -0500

    Firs version of challenge

--- TiendaPagoCodilityTest/Program.cs
+++ TiendaPagoCodilityTest/Program.cs
@@ -1,29 +1,37 @@
 ﻿public class Program
 {
     public static void Main(string[] args) {
-        Console.WriteLine(Solution("4 5 6 - 7 +"));
+        Console.WriteLine(Solution("4 5 6 - 7 + POP"));
     }
 
     private static int Solution(string S)
     {
         var a = S.Split(' ');
-
+        Stack<string> inputStack = new Stack<string>();
+        
         foreach (var item in a)
+        {
+            inputStack.Push(item);
+        }
+
+        foreach (var item in inputStack)
         {
             switch (item)
             {
-                case "POP":
-                    
+                case "POP":// Elimina el elemento anterior de la pila
+                    inputStack.Pop();
                     break;
-                case "DUP":// duplica el elemento anterior de la pila
+                case "DUP":// Duplica el elemento anterior de la pila
                     //a.GetValue(a.)
                     break;
-                case "+":
+                case "+":// Suma los ultimos 2
                     break;
-                case "-":
+                case "-":// Resta(Saca la diferencia) los ultimos 2
                     break;
             }
         }
+
+        Console.WriteLine($"Resultant stack: {string.Join(' ', inputStack)}");
 
         return a.Length;
     }
