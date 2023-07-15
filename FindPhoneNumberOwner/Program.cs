0a0cea2c03153662eefc601e47bc2ac32bf0bb22
bamartinezd <alejandromardelg@gmail.com>
2023-07-15 01:41:48 -0500

    Adding a new project

---
+++ FindPhoneNumberOwner/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+class Program {
+
+    public static void Main(string[] args){
+        string[] names ={"Brayan", "Alejandro", "David", "Andres"};
+        string[] numbers = {"3204796677", "3505256845", "3168297315", "3208922323"};
+
+        string res = solution(names, numbers, "320");
+        Console.WriteLine(res);
+    }
+
+    public static string solution(string[] A, string[] B, string P) {
+        var phoneNumbers = B.Where(x => x.Contains(P));
+        int position;
+        List<string> names= new List<string>();
+
+        foreach(string phoneNumber in phoneNumbers)
+        {
+            position = Array.IndexOf(B, phoneNumber);
+            if (position >= 0)
+            {
+                names.Add(A[position]);    
+            }
+            
+        }
+
+        if (names is not null || names.Count() > 0){
+            names.Sort();
+            return names.FirstOrDefault();
+        }else
+        {
+            return "NO CONTACT";
+        }
+        
+    }
+}
