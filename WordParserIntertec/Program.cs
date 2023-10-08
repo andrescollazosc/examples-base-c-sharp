03d179079cfd76b33d47291900a7511ba480b78f
bamartinezd <alejandromardelg@gmail.com>
2023-10-07 22:45:21 -0500

    Context of wordparserIntertec added

--- WordParserIntertec/Program.cs
+++ WordParserIntertec/Program.cs
@@ -9,6 +9,24 @@ A few of the things we will be looking at is accuracy, efficiency, solution comp
 */
 public class Program
 {
+    public static void Main()
+    {
+        /*
+    Write a program that parses a sentence and replaces each word with the following:
+        1) The first letter of the word
+        2) The number of distinct characters between first and last character
+        3) The last letter of the word.
+    For example, "Smooth" would become "S3h".
+    Words are separated by spaces or non-alphabetic characters and these separators should be maintained in their original form and location in the answer.
+    Input: "Creativity is thinking-up new things. Innovation is doing new things!"
+    Expected Output: "C6y is t4g-up n1w t4s. I6n is d3g n1w t4s!"
+    A few of the things we will be looking at is accuracy, efficiency, solution completeness.
+         */
+
+        string output = WordParser("Creativity is thinking-up new things. Innovation is doing new things!");
+        Console.WriteLine(output);
+        // expected: C6y is t4g-up n1w t4s. I6n is d3g n1w t4s!
+    }
     public static string WordParser(string input)
     {
         string output = string.Empty;
@@ -64,10 +82,4 @@ public class Program
         return countUniqueLetters.Equals("0") ? string.Empty : countUniqueLetters; 
     }
 
-    public static void Main()
-    {
-        string output = WordParser("Creativity is thinking-up new things. Innovation is doing new things!");
-        Console.WriteLine(output);
-        // expected: C6y is t4g-up n1w t4s. I6n is d3g n1w t4s!
-    }
 }
