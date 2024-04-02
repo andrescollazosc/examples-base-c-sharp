5972e090f278ad9cd6ee9121e7bf995833369ac6
bamartinezd <alejandromardelg@gmail.com>
2024-04-01 19:18:59 -0500

    feat(extra-long-factorials):Add new recursive approach to calculate factorials
     - Currently running extraLongFactorials function.
     - Rename extraLongFactorials function.
    
    Refs: US-AMD

---
+++ extra-long-factorials/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Numerics;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int n = 25;
+
+        
+        Console.WriteLine(CalculateFactorial(n));
+        //extraLongFactorials(n);
+    }
+
+    public static void extraLongFactorials(int n)
+    {
+        if (n == 0)
+            Console.WriteLine(n);
+
+        BigInteger factorial = n;
+        
+        while (n > 1)
+        {
+            n--;
+            factorial = factorial * n;
+        }
+
+        Console.WriteLine(factorial);
+    }
+
+    public static BigInteger CalculateFactorial(int n) {
+
+        if (n == 0 || n == 1)
+            return 1;
+        else
+        {
+            return CalculateFactorial(n - 1)*n;
+        }
+    } 
+}
